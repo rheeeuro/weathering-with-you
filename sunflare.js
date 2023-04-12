d3b8f6a8cee287ab3fb72cf6d4a7f03f8abd6f16
이유로 <eurohand@naver.com>
2023-04-12 11:48:44 +0900

    flare effect change

--- sunflare.js
+++ sunflare.js
@@ -1,13 +1,13 @@
 const sunshine = document.getElementById("sunshine");
 const hand = document.getElementById("hand");
-const effect = document.getElementById("flareEffect");
+// const effect = document.getElementById("flareEffect");
 
 sunshine.addEventListener("mouseenter", (e) => {
   hand.style.transform = `translate(-55%, -45%) scaleX(-1)`;
-  effect.style.opacity = 0.4;
+  // effect.style.opacity = 0.4;
 });
 
 sunshine.addEventListener("mouseleave", (e) => {
   hand.style.transform = `translate(-60%, -45%) scaleX(-1)`;
-  effect.style.opacity = 1;
+  // effect.style.opacity = 1;
 });
